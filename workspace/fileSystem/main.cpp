8509a5e40c1f27f2a8e82cb58169bc0a5dbfa151
Tatjana Isaković <tatjanai@telekom.yu>
2019-12-19 20:39:08 +0100

    Nothing is working, but it is compilinggit add .git add . :)

--- workspace/fileSystem/main.cpp
+++ workspace/fileSystem/main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 #include "kernelFS.h"
+#include "part.h"
+
 
 int main() {
+	std::cout << "Program started executing!" << std::endl;
+
+	Partition* p = new Partition((char *)"p1.ini");
+
 	KernelFS* k = new KernelFS();
+
+	std::cout << "Program finished executing!" << std::endl;
 }
